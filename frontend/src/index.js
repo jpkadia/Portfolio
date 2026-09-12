17d6592006dcf18b26f8efd1968df4a9598275cf
JP Kadiya <jpkadia9608@gmail.com>
2026-09-12 23:51:44 +0530

    fix: resolve router basename for vercel deployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const basename = (process.env.PUBLIC_URL && process.env.PUBLIC_URL.startsWith('/'))
+  ? process.env.PUBLIC_URL
+  : '';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
