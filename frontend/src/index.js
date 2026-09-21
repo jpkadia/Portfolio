86ca5a49afdae05d8f15f899956341d0ff6fe2ad
JP Kadiya <jpkadia9608@gmail.com>
2026-09-21 23:01:19 +0530

    Remove unused test, demo, and boilerplate files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const basename = (process.env.PUBLIC_URL && process.env.PUBLIC_URL.startsWith('/'))
   ? process.env.PUBLIC_URL
@@ -16,5 +15,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-reportWebVitals();
